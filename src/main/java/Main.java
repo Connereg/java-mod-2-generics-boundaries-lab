259c2ead25052af9e4d8ddbd515065a6e9ff5830
Conner Gleason <connereg@optonline.net>
2022-06-28 17:08:25 -0400

    Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Main {
     // implement the public averageList method here
+    public static Double averageList(List<? extends Number> inputList) {
+        double sum = 0.0;
+        double avgResult = 0.0;
+        for (Number n : inputList) {
+            sum += n.doubleValue();    
+        }
+        avgResult = sum / inputList.size();
+        return avgResult;
+    }
     
     public static void main(String[] args) {
         List<Integer> intList = List.of(1, 2, 3, 4, 5);
